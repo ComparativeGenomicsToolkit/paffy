0be708dcceb18c755eea78f8a8109ed0404cde65
benedictpaten <benedictpaten@gmail.com>
2023-04-02 19:09:06 -0700

    Futzing with parameters

--- tests/paf_pipeline_test.sh
+++ tests/paf_pipeline_test.sh
@@ -19,9 +19,14 @@ trap "rm -rf ${working_dir}" EXIT
 # Get the sequences
 wget https://raw.githubusercontent.com/ComparativeGenomicsToolkit/cactusTestData/master/T2T_primate_PAR/mPanPan1_XY_1_5000000.fa -O ${working_dir}/mPanPan1_XY_1_5000000.fa
 wget https://raw.githubusercontent.com/ComparativeGenomicsToolkit/cactusTestData/master/T2T_primate_PAR/mPanTro3_XY_1_5000000.fa -O ${working_dir}/mPanTro3_XY_1_5000000.fa
+#wget https://raw.githubusercontent.com/UCSantaCruzComputationalGenomicsLab/cactusTestData/master/evolver/mammals/loci1/simCow.chr6 -O ${working_dir}/simCow.chr6.fa
+#wget https://raw.githubusercontent.com/UCSantaCruzComputationalGenomicsLab/cactusTestData/master/evolver/mammals/loci1/simDog.chr6 -O ${working_dir}/simDog.chr6.fa
+
 
 # Run lastz
 lastz ${working_dir}/mPanPan1_XY_1_5000000.fa[multiple][nameparse=darkspace] ${working_dir}/mPanTro3_XY_1_5000000.fa[multiple][nameparse=darkspace] --step=4 --ambiguous=iupac,100,100 --ydrop=3000 --format=paf:minimap2 > ${working_dir}/lastz.paf
+#lastz ${working_dir}/simCow.chr6.fa[multiple][nameparse=darkspace] ${working_dir}/simDog.chr6.fa[multiple][nameparse=darkspace] --step=4 --ambiguous=iupac,100,100 --ydrop=3000 --format=paf:minimap2 > ${working_dir}/lastz.paf
+
 
 # Inverting so for every query:target alignment we have the mirror target:query alignment, this is used to
 # make alignments symmetric for chaining
